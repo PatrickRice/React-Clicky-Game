85b1e4ccaf8a7a8d5cae3b816b7bcbf029962849
Pat Rice <patrickrice@Patricks-MBP.hsd1.ca.comcast.net>
2019-02-18 02:46:29 -0800

    Updates

---
+++ precache-manifest.4682f8382f88b5372575820c50e74d69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e410ef131087a5d5cfb",
+    "url": "/React-Clicky-Game/static/js/runtime~main.4e410ef1.js"
+  },
+  {
+    "revision": "ba016b95eef501961b5b",
+    "url": "/React-Clicky-Game/static/js/main.ba016b95.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/React-Clicky-Game/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "ba016b95eef501961b5b",
+    "url": "/React-Clicky-Game/static/css/main.dc58ecd1.chunk.css"
+  },
+  {
+    "revision": "a297dfa0f77624821ae3a9ea3213d114",
+    "url": "/React-Clicky-Game/index.html"
+  }
+];
